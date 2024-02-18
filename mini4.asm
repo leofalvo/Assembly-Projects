cdc1e27e90357e9eda61b2ff1ded556971f2e298
lfal703 <146048320+lfal703@users.noreply.github.com>
2024-02-18 17:30:22 -0500

    Finished drivers

--- mini4.asm
+++ mini4.asm
@@ -1,17 +1,29 @@
 .data
-	
+	getbuffer: .space 10
 .text
 .globl main
 main:
 	
-	jal gets
-	la $v0, ($a0)
-	li $v0, 4
-	syscall
 	
-	li $v0, 10 # End Program
-	syscall
-	
+	puts:
+		# int puts(char *buffer)
+		addi $sp, $sp -4 # Decrement Sp
+		sw $ra 0($sp) # Save ra to sp
+		la $t0, ($a0) # Load input buffer
+		
+		puts_loop:
+			lb $t1, 0($t0) # Load current byte
+			beqz $t1, puts_end # End if zero
+			move $a0, $t1 # Move to input
+			jal PutChar # Print current byte
+			addi $t0, $t0, 1 # Move to next one
+			j puts_loop # Loop
+		
+		puts_end:
+		lw $ra, 0($sp) # Restore $ra
+		addi $sp, $sp 4 # Restore sp
+		jr $ra # Return
+			
 	
 	gets:
 		# int gets(char *buffer, int limit)
@@ -23,22 +35,26 @@ main:
 		
 		move $t0, $a0 # Buffer
 		li $t2, 0 # Character counter
-		li $t3, 0 # Read char counter
 		
 		gets_loop:
 			jal GetChar
 			sb $v0, 0($t0) # store char in buffer
 			addi $t0, $t0, 1 # Increment buffer
-			addi $t2, $t2, 1 # Incrememnt char count
-			addi $t3, $t3, 1 # incremeant read
-			beq $v0, 13, finish # If enter, leave
+			addi $t2, $t2, 1 # Increment char count
+			beq $v0, 10, newline_finish # If enter, leave
 			bge $t2, $a1, finish # If limit, leave
 			j gets_loop
 		
-		finish:
+		newline_finish:
 		sb $zero, 0($t0)
-		move $v0, $t3
+		addi $t2, $t2, -1
 		
+		finish:
+		move $v0, $t2
+		j end
+		
+		
+		end:
 		lw $t2, 8($sp) # Restore $t2
 		lw $t0, 4($sp) # Restore $t0
 		lw $ra, 0($sp) # Restore $ra
@@ -46,7 +62,6 @@ main:
 		jr $ra # Return
 
 	
-	loop:
 		
 		
 		
@@ -63,6 +78,7 @@ main:
 		lw $t1, 0($a3) #read from receiver control reg
 		andi $t1, $t1, 0x1 #extract ready bit
 		beqz $t1, CkReady #if 1, then load char, else loop
+		lw $v0, 4($a3)
 		lw $ra 0($sp) # Load return address 
 		addi $sp, $sp, 4 # Reincrement stack pointer
 		jr $ra #return to place in program
@@ -77,6 +93,7 @@ main:
 		andi $t1, $t1, 0x1 #extract ready bit
 		beqz $t1, XReady #if 1, then store char, else loop
 		sw $a0, 12($a3) #send character to display
+		
 		lw $ra 0($sp) # Load return address 
 		addi $sp, $sp, 4 # Reincrement stack pointer
 		jr $ra #return to place in program before
